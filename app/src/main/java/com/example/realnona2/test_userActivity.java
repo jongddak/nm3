2b05fe144a9e0dc55346cca81433a05dbfb88c6a
jongddak <hfsmsj316@gmail.com>
2022-12-04 17:16:41 +0900

    인트로, 로그인 로고 수정

--- app/src/main/java/com/example/realnona2/test_userActivity.java
+++ app/src/main/java/com/example/realnona2/test_userActivity.java
@@ -15,10 +15,6 @@ public class test_userActivity extends AppCompatActivity {
     private Button Logout,testbtn;
     private Context mContext;
 
-
-    /*회원가입 페이지로 정보 보낼려면 겟셋 두번 하기 or 한번에 보내주는 법 찾기 */
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,38 +26,30 @@ public class test_userActivity extends AppCompatActivity {
         UserinfoSex = findViewById(R.id.UserinfoSex);
         testbtn= findViewById(R.id.testbtn);
         mContext = this;
-
         Logout = findViewById(R.id.Logout);
 
         String Userdata1 = Loginsave.getString(mContext,"UserNum");
         String Userdata2 = Loginsave.getString(mContext,"UserName");
         String Userdata3 = Loginsave.getString(mContext,"UserClass");
         String Userdata4 = Loginsave.getString(mContext,"UserSex");
-        /*
-        Intent intent = getIntent();
-        final String data1 = intent.getStringExtra("UserNum");
-        final String data2 = intent.getStringExtra("UserName");
-        final String data3 = intent.getStringExtra("UserClass");
-        final String data4 = intent.getStringExtra("UserSex");
-*/
-        testbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                UserinfoNum.setText(Userdata1);
-                UserinfoName.setText(Userdata2);
-                UserinfoClass.setText(Userdata3);
-                UserinfoSex.setText(Userdata4);
-
-            }
-        });
 
         UserinfoNum.setText(Userdata1);
         UserinfoName.setText(Userdata2);
         UserinfoClass.setText(Userdata3);
         UserinfoSex.setText(Userdata4);
 
+        testbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*회원탈퇴 들어갈 부분*/
+
+            }
+        });
 
 
+
+
+      //로그아웃 버튼
         Logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
